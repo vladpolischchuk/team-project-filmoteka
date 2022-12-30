9f124cd4f6ece60dc1ff816bc822ed500adc5207
Karlushyna Lesya <karlushyna.l@gmail.com>
2022-12-30 13:39:17 +0000

    add modal-film js
    intermediate stage of js,
    film modal window opens with temporary button(situated under footer),
    tasks to solve: close modal  with click on space , out of modal window, open modal with click on card, loading data from back-end

---
+++ src/js/modal-film.js
@@ -0,0 +1,22 @@
+
+    const refs = {
+        openModalBtn: document.querySelectorAll('[data-film-modal-open]'),
+        closeModalBtn: document.querySelector('[data-film-modal-close]'),
+        modal: document.querySelector('[data-film-modal]'),
+      };
+    
+      refs.openModalBtn.forEach(e => {
+        e.addEventListener('click', toggleModal)
+      });
+      refs.closeModalBtn.addEventListener('click', toggleModal);
+    
+      function toggleModal() {
+        refs.modal.classList.toggle('is-hidden');
+      }
+    ;
+  
+  document.addEventListener('keydown', function(e) {
+      if (e.key === 'Escape') {
+      toggleModal()
+      }
+      });
