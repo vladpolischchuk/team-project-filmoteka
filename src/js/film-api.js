70584b9f55c73eb7b48f8f2d196cacd7fbea6c70
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2022-12-30 19:21:19 +0200

    adds fetch for movie info

--- src/js/film-api.js
+++ src/js/film-api.js
@@ -1,6 +1,8 @@
 const URL = 'https://api.themoviedb.org/3';
 const KEY = 'cf961b1b89f4c4a28558be2b04fdd59a';
 
+//EXPORT  FUNCTIONS
+export { fetchFilmsAPI, fetchGenresAPI, fetchMovieInfoAPI};
 // FETCH FOR MOVIE OF THE DAY
 async function fetchFilmsAPI(page=1) {
     return await
@@ -40,5 +42,23 @@ async function fetchGenresAPI() {
 }
 //FUNCTION CALL
 //fetchGenresAPI();
-//EXPORT TWO FUNCTIONS
-export { fetchFilmsAPI, fetchGenresAPI };
+
+// FETCH FOR MOVIE INFO
+async function fetchMovieInfoAPI(movie_id) {
+    return await
+        fetch(`${URL}/movie/${movie_id}?api_key=${KEY}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not OK');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            return data;
+        })
+        .catch((error) => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+}
+//FUNCTION CALL
+//fetchMovieInfoAPI(8);
