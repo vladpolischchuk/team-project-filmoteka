767759ccd5428953303ba299dacd74b50c176e2c
E-gap <106596181+E-gap@users.noreply.github.com>
2023-01-03 21:15:13 +0200

    fix pagination for genresSearch

--- src/js/trending-films-render.js
+++ src/js/trending-films-render.js
@@ -1,7 +1,12 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 
-import { fetchFilmsAPI, fetchMoreFilmsAPI, fetchGenresAPI } from './film-api';
+import {
+  fetchFilmsAPI,
+  fetchMoreFilmsAPI,
+  fetchMoreGenresAPI,
+  fetchGenresAPI,
+} from './film-api';
 
 export { createCardMarkup };
 
@@ -33,46 +38,40 @@ const options = {
 };
 
 const refs = {
-    movieList: document.querySelector('.home'),
-    pagination: document.querySelector('.tui-pagination'),
-}
+  movieList: document.querySelector('.home'),
+  pagination: document.querySelector('.tui-pagination'),
+};
 
 const basicImgURL = 'https://image.tmdb.org/t/p/w500';
 const pagination = new Pagination(refs.pagination, options);
 const page = pagination.getCurrentPage();
 
 fetchGenresAPI().then(genres => {
-    fetchFilmsAPI(page).then(data => {
-        let markup = createCardMarkup(data, genres);
-        refs.movieList.insertAdjacentHTML('beforeend', markup);
-    });
+  fetchFilmsAPI(page).then(data => {
+    let markup = createCardMarkup(data, genres);
+    refs.movieList.insertAdjacentHTML('beforeend', markup);
+  });
 });
 
-pagination.on('afterMove', fetchMoreFilmsAPI);
+//pagination.on('afterMove', fetchMoreGenresAPI);
 
 function createCardMarkup(data, genres_names) {
-    if (data.length === 0) {
-        return;
-    }
-    return data.map((
-        {
-            title,
-            poster_path,
-            release_date,
-            genre_ids,
-        }
-    ) => {
-        let release = release_date.slice(0, 4);
-        let genres = [];
-        for (let genre_id of genre_ids) {
-            let genre = genres_names.find(({id}) => id === genre_id);
-            genres.push(genre.name);
-        }
-        if (genres.length >= 3) {
-            genres = [genres[0], genres[1], 'Other'];
-        }
-        let genres_str = genres.join(', ');
-        return `<li class="card">
+  if (data.length === 0) {
+    return;
+  }
+  return data
+    .map(({ title, poster_path, release_date, genre_ids }) => {
+      let release = release_date.slice(0, 4);
+      let genres = [];
+      for (let genre_id of genre_ids) {
+        let genre = genres_names.find(({ id }) => id === genre_id);
+        genres.push(genre.name);
+      }
+      if (genres.length >= 3) {
+        genres = [genres[0], genres[1], 'Other'];
+      }
+      let genres_str = genres.join(', ');
+      return `<li class="card">
   <a href="" class="card-link link">
     <img class="card-img" src="${basicImgURL}${poster_path}" alt="${title}">
     <h3 class="card-name">${title}</h3>
