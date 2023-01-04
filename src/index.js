8cf32f5be48c26e170e7ac1b218531001be77334
Vladyslav Polishchuk <105670228+vladpolischchuk@users.noreply.github.com>
2023-01-04 19:08:08 +0200

    adds modal window our team

--- src/index.js
+++ src/index.js
@@ -5,3 +5,7 @@ import './js/trending-films-render';
 import './js/notiflix';
 
 import './js/modal-film';
+
+import './js/open-md-win-our-team';
+
+import './js/slider';
---
+++ src/js/slider.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.list-taem').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2500
+    })
+});
